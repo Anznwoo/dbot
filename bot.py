c4520f43cd51aeabf85265f85144d85007e2b7cf
Anznwoo <tidls101068@gmail.com>
2022-01-08 22:26:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,15 @@ client = commands.Bot(command_prefix = '-')
 @client.event
 async def on_ready():
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
+    # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+    await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="퀴리 진행 준비"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+    await client.change_presence(activity=discord.Game(name="퀴리 진행 준비"))
+    #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+    #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+    #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
   
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+    print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 async def on_message(message):
     # SENDER가 BOT일 경우 반응을 하지 않도록 한다.
@@ -29,6 +29,13 @@ async def on_message(message):
         # 답변 내용 구성
         msg = "너도 바보"
         # msg에 지정된 내용대로 메시지를 전송
+        await channel.send(msg)
+        return None
+
+    if message.content == "날 태그해 줘":
+        channel = message.channel
+
+        msg = "<@{}>".format(message.author.id)
         await channel.send(msg)
         return None
 
